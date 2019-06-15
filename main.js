aa5abc92182becae6ec945bb90d90b29a81ff331
Pádraig Galvin <padraig@agence-b.fr>
2019-06-15 02:31:01 +0200

    Add MediaWiki output format and toggle buttons

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 
-const FORMAT_HTML = 'html';
-
 /**
  * Translation rules
  *
@@ -16,49 +14,65 @@ const rules = [
     // Match anything wrapped by double underscores
     pattern: /__(.+?)__/gm,
     html: '<strong>$1</strong>',
+    wiki: "'''$1'''",
   },
   // Strong text - double stars
   {
     // Match anything wrapped by double stars
     pattern: /\*\*(.+?)\*\*/gm,
     html: '<strong>$1</strong>',
+    wiki: "'''$1'''",
   },
   // Emphasized text - single underscores
   {
     // Match anything wrapped by single underscores
     pattern: /_(.+?)_/gm,
     html: '<em>$1</em>',
+    wiki: "''$1''",
   },
   // Emphasized text - single stars
   {
     // Match anything wrapped by single stars
     pattern: /\*(.+?)\*/gm,
     html: '<em>$1</em>',
+    wiki: "''$1''",
   },
   // Heading 1 - single hash
   {
     // Match exactly one hash with optional trailing hashes and whitespace
     pattern: /^\#(?!\#)[ \t]*(.+?)[ \t]*(?:\#+)?[ \t]*$/gm,
     html: '<h1>$1</h1>',
+    wiki: '= $1 =',
   },
   // Heading 2 - double hash
   {
     // Match exactly two hashes with optional trailing hashes and whitespace
     pattern: /^\#{2}(?!\#)[ \t]*(.+?)[ \t]*(?:\#+)?[ \t]*$/gm,
     html: '<h2>$1</h2>',
+    wiki: '== $1 ==',
   },
   // Heading 3 - triple hash
   {
     // Match exactly three hashes with optional trailing hashes and whitespace
     pattern: /^\#{3}(?!\#)[ \t]*(.+?)[ \t]*(?:\#+)?[ \t]*$/gm,
     html: '<h3>$1</h3>',
+    wiki: '=== $1 ===',
   },
   // Paragraph - double newline
   {
     pattern: /(.*?)(?:(\r|\r?\n){2})/gs,
     html: '<p>$1</p>',
+    wiki: '$1\n\n',
   },
 ];
+
+/**
+ * Available destination formats
+ *
+ * Each rule above must have a corresponding renderer.
+ */
+const FORMAT_HTML = 'html';
+const FORMAT_WIKI = 'wiki';
 
 /**
  * Escape HTML
@@ -79,7 +93,7 @@ function escapeHtml(unsafe){
  * Converts Markdown to requested format.
  */
 function render(markdown, format) {
-  if (format !== FORMAT_HTML) {
+  if (format !== FORMAT_HTML && format !== FORMAT_WIKI) {
     throw new Error('Unable to render unknown format');
   }
 
@@ -97,11 +111,42 @@ document.addEventListener('DOMContentLoaded', () => {
   const input = document.getElementById('input');
   const output = document.getElementById('output');
 
+  const buttonRenderHtml = document.getElementById('render-html');
+  const buttonRenderWiki = document.getElementById('render-wiki');
+
+  // Set default format
+  let outputFormat = FORMAT_HTML;
+  buttonRenderHtml.disabled = true;
+
+  // Pass input to outup
+  function display() {
+    let content = render(input.value, outputFormat);
+    // Preserve fromatting for text based format
+    if (outputFormat === FORMAT_WIKI) {
+      content = `<pre>${content}</pre>`;
+    }
+    output.innerHTML = content;
+  }
+
   // Autofocus on input for usability
   input.focus();
 
   // Automatically refresh rendered content when typing
-  input.addEventListener('keyup', () => {
-    output.innerHTML = render(input.value, FORMAT_HTML);
+  input.addEventListener('keyup', display);
+
+  // Switch output format to HTML
+  buttonRenderHtml.addEventListener('click', () => {
+    buttonRenderHtml.disabled = true;
+    buttonRenderWiki.disabled = false;
+    outputFormat = FORMAT_HTML;
+    display();
+  });
+
+  // Switch output format to MediaWiki
+  buttonRenderWiki.addEventListener('click', () => {
+    buttonRenderHtml.disabled = false;
+    buttonRenderWiki.disabled = true;
+    outputFormat = FORMAT_WIKI;
+    display();
   });
 });
